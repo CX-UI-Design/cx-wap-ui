b6739b51382e5c11ed0d911f33e93418e615a4b1
xilanhuadechuntian <Lensgcx@163.com>
2018-12-26 17:08:37 +0800

    Actionsheet
    Actionsheet

---
+++ src/packages/Actionsheet/index.js
@@ -0,0 +1,4 @@
+import _Actionsheet from './src/Actionsheet'
+import './src/style/index.scss'
+
+export default _Actionsheet
---
+++ src/packages/Actionsheet/src/Actionsheet.vue
@@ -0,0 +1,53 @@
+<!--架构组 - 基础组件调试 - Actionsheet 上拉菜单 组件-->
+<template>
+  <van-actionsheet
+    class="ns-actionsheet"
+    :actions="actions"
+    :title="title"
+    :cancel-text="cancelText"
+    :overlay="overlay"
+    :close-on-click-overlay="closeOnClickOverlay"
+    :lazyRender="lazyRender"
+    :getContainer="getContainer"
+    @select="actionsheet_select"
+    @cancel="actionsheet_cancel"
+  >
+  </van-actionsheet>
+</template>
+
+<script>
+  import create from '../../../utils/create';
+
+  export default create({
+    name: 'actionsheet',
+    props: {
+      actions: {
+        type: Array, default: function () {
+          return []
+        }
+      },//菜单选项
+      title: {type: String},//标题
+      cancelText: {type: String},//取消按钮文字，为空时不展示取消按钮
+      overlay: {type: Boolean, default: true},//是否显示遮罩层
+      closeOnClickOverlay: {type: Boolean, default: true},//是否在点击蒙层后关闭
+      lazyRender: {type: Boolean, default: true},//是否在显示弹层时才渲染节点
+      getContainer: {type: [String, Function]},//指定挂载的节点，可以传入选择器，或一个返回节点的函数
+    },
+    methods: {
+      /**
+       * select - 选中选项时触发，禁用或加载状态下不会触发
+       * @param value ( 选项对应的对象 )
+       */
+      actionsheet_select(value) {
+        this.$emit('click', value);
+      },
+      actionsheet_cancel() {
+        this.$emit('click');
+      }
+    }
+  })
+</script>
+
+<style rel="stylesheet/scss" lang="scss">
+
+</style>
