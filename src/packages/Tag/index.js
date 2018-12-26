019471042739572360ee9f235cf70df8a1ba44fe
xilanhuadechuntian <Lensgcx@163.com>
2018-12-26 12:01:40 +0800

    Tag
    Tag

---
+++ src/packages/Tag/index.js
@@ -0,0 +1,4 @@
+import _Tag from './src/Tag'
+import './src/style/index.scss'
+
+export default _Tag
---
+++ src/packages/Tag/src/Tag.vue
@@ -0,0 +1,28 @@
+<!--架构组 - 基础组件调试 - Tag 标记 组件-->
+<template>
+  <van-tag class="ns-tag" :type="type" :size="size" :color="color" :plain="plain"
+           :round="round" :mark="mark" :textColor="textColor">
+    <slot></slot>
+  </van-tag>
+</template>
+
+<script>
+  import create from '../../../utils/create';
+
+  export default create({
+    name: 'tag',
+    props: {
+      type: {type: String},//primary success danger
+      size: {type: String},//large medium
+      color: {type: String},
+      plain: {type: Boolean, default: false},
+      round: {type: Boolean, default: false},
+      mark: {type: Boolean, default: false},
+      textColor: {type: String, default: 'white'}
+    }
+  })
+</script>
+
+<style rel="stylesheet/scss" lang="scss">
+
+</style>
