db6adc1bf8a3421739d59400969b6b010bc4fa4a
xilanhuadechuntian <Lensgcx@163.com>
2018-12-29 10:52:29 +0800

    run index

---
+++ build/run/index.js
@@ -0,0 +1,28 @@
+/**
+ * Build npm src
+ */
+const shell = require('shelljs');
+const signale = require('signale');
+
+const {Signale} = signale;
+const tasks = [
+  'bootstrap',
+  'lint',
+  'build:entry',//生成总入口文件
+  'build:components-entry',//生成各个组件的入口文件
+  'build:components',//生成 lib es 文件夹目录及其文件
+  'del:components-entry',//删除各个组件的入口文件
+  'build:style',
+  'build:style-entry',
+  'build:vant',
+  // 'build:copy'
+];
+
+tasks.forEach(task => {
+  signale.start(task);
+
+  const interactive = new Signale({interactive: true});
+  interactive.pending(task);
+  shell.exec(`npm run ${task} --silent`);
+  interactive.success(task);
+});
