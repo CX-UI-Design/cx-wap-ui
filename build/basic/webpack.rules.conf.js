be635f04bb48a093934858b2429b9cf01a72ce0c
xilanhuadechuntian <Lensgcx@163.com>
2018-12-29 10:51:05 +0800

    webpack.rules.conf
    webpack.rules.conf

---
+++ build/basic/webpack.rules.conf.js
@@ -0,0 +1,49 @@
+const path = require('path');
+
+const rules = [
+  {
+    test: /\.vue$/,
+    use: [
+      {
+        loader: 'vue-loader',
+        options: {
+          compilerOptions: {
+            preserveWhitespace: false
+          }
+        }
+      }
+    ]
+  },
+  {
+    test: /\.js$/,
+    exclude: /node_modules/,
+    use: 'babel-loader'
+  },
+  {
+    test: /\.less$/,
+    use: [
+      'style-loader',
+      'css-loader',
+      'postcss-loader',
+      {
+        loader: 'less-loader',
+        options: {
+          paths: [path.resolve(__dirname, 'node_modules')]
+        }
+      }
+    ]
+  },
+  {
+    test: /\.md$/,
+    use: [
+      'vue-loader',
+      '@vant/markdown-loader'
+    ]
+  },
+  {
+    test: /\.(ttf|svg)$/,
+    loader: 'url-loader'
+  }
+];
+
+module.exports = rules;
