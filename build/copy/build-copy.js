51ab7af4b02970597c2db2cf8b1e5b0df714fd50
xilanhuadechuntian <Lensgcx@163.com>
2019-01-08 10:12:54 +0800

    build-copy
    build-copy

--- build/copy/build-copy.js
+++ build/copy/build-copy.js
@@ -5,8 +5,8 @@ var notify = require('gulp-notify');
 var clean = require('gulp-clean');
 
 
-var targetPath = path.join(__dirname, '../lib');
-var toPath = '../../CX-WAP-Design/';
+var targetPath = path.join(__dirname, '../../lib');
+var toPath = '../../../CX-WAP-Design/';
 var fileName = 'cx-wap-ui';
 
 gulp.task('clean-lib', function () {
